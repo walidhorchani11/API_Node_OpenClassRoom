03cacd6e2a92237cbc12c107a1ce1b111c29eae2
walid horchani <walid.horchani@oyez.fr>
2020-02-13 12:00:59 +0100

    amelioration server - errorHandler

--- server.js
+++ server.js
@@ -21,6 +21,35 @@ const port = normalizePort(process.env.PORT || '3000');
 //set port 
 app.set('port', port);
 
+//error handler
+const errorHandler = error => {
+    if(error.syscall !== 'listen'){
+        throw error;
+    }
+
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe' + address : 'port' + port;
+    switch (error.code) {
+        case 'EACCES':
+          console.error(bind + 'require elevated privileges.');
+          process.exit(1);
+          break;
+        case 'EADDRINUSE':
+          console.error(bind + 'is already in use.');
+          process.exit(1);
+          break;
+        default:
+          throw error;
+    }
+}
+
 const server = http.createServer(app);
 
-server.listen(process.env.PORT || 3000);
+server.on('error', errorHandler);
+server.on('listening', ()=> {
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+    console.log('Listening on ' + bind);
+})
+
+server.listen(port);
