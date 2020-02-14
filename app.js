4be1b349c48692b8fc840027a34fc3a505ac3cf3
walid horchani <walid.horchani@oyez.fr>
2020-02-14 16:19:38 +0100

    updateOne

--- app.js
+++ app.js
@@ -62,4 +62,10 @@ app.delete('/api/stuff/:id',(req, res, next) => {
     .catch(error => {res.status(400).json(error)});
 })
 
+app.put('/api/stuff/:id', (req, res, next) => {
+  Thing.updateOne({_id: req.params.id}, { ...req.body, _id: req.params.id} )
+    .then(() =>  res.status(200).json({message: 'Objet modifié !!'}))
+    .catch(error => res.status(400).json({error}));
+});
+
 module.exports = app;
