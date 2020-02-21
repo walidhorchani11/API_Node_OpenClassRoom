f9552ecbe3993ec53cf2c2dbc9bcb9f6da78b474
walid horchani <walid.horchani@oyez.fr>
2020-02-21 10:01:19 +0100

    export config multer

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -4,17 +4,20 @@ const MIME_TYPES = {
   'image/jpg': 'jpg',
   'image/jpeg': 'jpg',
   'image/png': 'png'
-}
+};
 
 //creer un objet de configuration
 const storage = multer.diskStorage({
+  
   destination: (req, file, callback) => {
     //premiere argument null , pour dire qu'il n ya pas d'erreur à ce niveau la 
-    callback(null, 'images')
+    callback(null, 'images');
+    
   },
   //il faut le faire pour eviter de mettre les noms d'origine, en cas ou deux noms seront identiques
   filename: (req, file, callback) => {
     //il faut enlever les espaces car il cause probleme eu serveur, et de les remplacer par exp par _
+   
     const name = file.originalname.split(' ').join('_');
     //creer l extension selon le mimetypes du file
     const extension = MIME_TYPES[file.mimetype];
@@ -23,3 +26,5 @@ const storage = multer.diskStorage({
     callback(null, name + Date.now() + '.' + extension);
   }
 });
+
+module.exports = multer({storage: storage}).single('image');
