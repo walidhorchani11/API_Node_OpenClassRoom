ee2e08dbabbf568d24104b9789cf5c1ea8653adb
walid horchani <walid.horchani@oyez.fr>
2020-02-21 10:04:01 +0100

    apply middleware multer to post and put route to handle file name and location

--- routes/stuff.js
+++ routes/stuff.js
@@ -3,12 +3,13 @@ const router = express.Router();
 const stuffCtrl = require('../controllers/stuff');
 //importer le middleware pour securiser les routes voulu
 const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
 
 //ajouter  le middleware auth pour appliquer la securiter au routes
-router.post('/',auth, stuffCtrl.creatThing);
+router.post('/',auth, multer, stuffCtrl.creatThing);
 router.get('/:id',auth, stuffCtrl.getOneThing);
 router.get('/',auth, stuffCtrl.getThings);
 router.delete('/:id',auth, stuffCtrl.deleteThing);
-router.put('/:id',auth, stuffCtrl.updateThing);
+router.put('/:id',auth, multer, stuffCtrl.updateThing);
 
 module.exports = router;
